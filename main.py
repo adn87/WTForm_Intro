0a84f2b24da684d363ad3aeb5e0411e207832ac3
adn87 <aaghadieh@gmail.com>
2023-12-02 10:44:12 -0500

    WTForm_Intro_004
    - Receiving Form Data with WTForms.

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ def home():
 def login():
     login_form = MyForm()
     login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
 
 
